9c5e441b3a22094826eb9022774417ad9bc6f981
Durian Manuel Sosa da Silva <durian.sosa@refinery89.com>
2025-09-03 12:54:15 +0200

    drum stilieren

---
+++ src/components/Widgets/FccLogo.tsx
@@ -0,0 +1,11 @@
+import { FaFreeCodeCamp } from "react-icons/fa";
+
+function FccLogo() {
+    return (
+<div className="logo">
+                <FaFreeCodeCamp />
+                <span className='title'>preview</span>
+              </div>
+    );
+}
+export default FccLogo;
