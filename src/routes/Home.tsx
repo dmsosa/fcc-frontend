a0dddb5a2d1cfed807a57e5efef4b4665ee272a6
dmsosa <dmsosa21@outlook.com>
2025-12-19 19:02:37 +0100

    main scroll snap

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -20,8 +20,8 @@ export default function Home () {
     
     return (
         <>
-        <section className="section h-100vh overflow-hidden">
-            <div className='container scroll-snap-child min-h-100 bg-body-primary invisible-scrollbar'>
+        <section className="section invisible-scrollbar">
+            <div className='container bg-body-primary'>
                 <div className="row">
                     <h1>
                         <span>Hi,</span>
@@ -40,47 +40,11 @@ export default function Home () {
                 <div className="row">
                     <p>Lass mich wissen, ob es cool war!</p>
                 </div>              
-            </div>
-            <div className='container scroll-snap-child min-h-100 bg-body-secondary invisible-scrollbar'>
-                <div className="row">
-                    <h1>Free Code Camp: Frontend Challenges</h1>
-                    <p className="swatch-secondary">Welcome to my collection of frontend challenges from Free Code Camp. Here, you'll find a variety of projects that showcase my skills in HTML, CSS, and JavaScript. Each challenge is designed to test different aspects of frontend development, from responsive design to interactive features.</p>
-                    <p>Feel free to explore the projects and see how I've approached each challenge. Whether you're a fellow developer looking for inspiration or someone interested in learning more about frontend development, I hope you find these challenges both informative and engaging.</p>
-                    <p>Happy coding!</p>
-                </div>
-                <div className="row">
-                    <div className="d-flex justify-content-center align-items-start flex-wrap gap-2">
-                        {PROJECTS_ARRAY.map((p: TProject) => {
-                            return (<ProjektKarte key={p.title} project={p}/>)
-                        })}
-                    </div>
-                </div>
-                <div className="row">
-                    <p>Lass mich wissen, ob es cool war!</p>
-                </div>              
-            </div>
-            <div className='container scroll-snap-child min-h-100 bg-body-primary invisible-scrollbar'>
-                <div className="row">
-                    <h1>Free Code Camp: Frontend Challenges</h1>
-                    <p className="swatch-secondary">Welcome to my collection of frontend challenges from Free Code Camp. Here, you'll find a variety of projects that showcase my skills in HTML, CSS, and JavaScript. Each challenge is designed to test different aspects of frontend development, from responsive design to interactive features.</p>
-                    <p>Feel free to explore the projects and see how I've approached each challenge. Whether you're a fellow developer looking for inspiration or someone interested in learning more about frontend development, I hope you find these challenges both informative and engaging.</p>
-                    <p>Happy coding!</p>
-                </div>
-                <div className="row">
-                    <div className="d-flex justify-content-center bg-body-secondary align-items-start flex-wrap gap-2">
-                        {PROJECTS_ARRAY.map((p: TProject) => {
-                            return (<ProjektKarte key={p.title} project={p}/>)
-                        })}
-                    </div>
-                </div>
-                <div className="row">
-                    <p>Lass mich wissen, ob es cool war!</p>
-                </div>              
             </div>       
         </section>
-        <section id='beschreibung' className='h-100vh bg1 scroll-snap-child'></section>
-        <section id='beschreibung' className='h-100vh bg2 scroll-snap-child'></section>
-        <section id='beschreibung' className='h-100vh bg3 scroll-snap-child'></section>
+        <section className='section min-vh-100 bg-primary'></section>
+        <section className='section min-vh-100 bg-secondary'></section>
+        <section className='section min-vh-100 bg-body-tertiary'></section>
         </>
 
     
